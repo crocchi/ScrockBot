57c1819e42ca0baf49e52300a1f055f5cf76d3fe
crocchi <ziocro@gmail.com>
2024-10-19 15:13:59 +0200

    Create market.js

---
+++ market.js
@@ -0,0 +1,6 @@
+const solanaWeb3 = require('@solana/web3.js');
+const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
+
+const solUsdc = "4DoNfFBfF7UokCC2FQzriy7yHK6DY6NVdYpuekQ5pRgg";
+const marketAddress = new solanaWeb3.PublicKey("INDIRIZZO_MARKET_SOL_USDC");
+

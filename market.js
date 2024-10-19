9ce01798e4dac4c82b03661efb638facfc657faf
crocchi <ziocro@gmail.com>
2024-10-19 17:53:24 +0200

    Update market.js

--- market.js
+++ market.js
@@ -1,4 +1,6 @@
 const solanaWeb3 = require('@solana/web3.js');
+const { Market } = require('@project-serum/serum');
+
 const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
 
 const solUsdc = "4DoNfFBfF7UokCC2FQzriy7yHK6DY6NVdYpuekQ5pRgg";
@@ -6,6 +8,18 @@ const marketAddress = new solanaWeb3.PublicKey(solUsdc);
 
 async function getMarketData() {
     // Ottieni l'account dei dati del mercato
+    const market = await Market.load(connection, marketPublicKey, {}, solanaWeb3.PublicKey.default);
+    const bids = await market.loadBids(connection);
+    const asks = await market.loadAsks(connection);
+
+    // Visualizza i migliori ordini di acquisto e vendita
+    for (let order of asks) {
+        console.log('Ask Price:', order.price, 'Size:', order.size);
+    }
+    
+    for (let order of bids) {
+        console.log('Bid Price:', order.price, 'Size:', order.size);
+    }
     const accountInfo = await connection.getAccountInfo(marketAddress);
     if (accountInfo === null) {
         console.log("Nessun account trovato per l'indirizzo fornito.");

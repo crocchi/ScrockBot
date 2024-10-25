1966191fb0c20073b5458e39d11956e144023499
crocchi <ziocro@gmail.com>
2024-10-25 12:40:35 +0200

    Update market.js

--- market.js
+++ market.js
@@ -9,7 +9,9 @@ const solanaChainstack='https://solana-mainnet.core.chainstack.com/78996579e7153
 // Inizio del timer
 console.time('timeNodeConnect');
 
-const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'),{wsEndpoint:solanaNodeWebSocket})
+//const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'),{wsEndpoint:solanaNodeWebSocket})
+
+const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'))
 
 console.log('connessa al nodo..');
 console.timeEnd('timeNodeConnect');
